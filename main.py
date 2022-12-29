0d886a08aeaf82e1adaa0b9d4e150f64e60399e0
Altán Cabal <altancabal@users.noreply.github.com>
2022-12-29 17:07:19 +0000

    Use a list of URLs in axiom instead of requesting one by one

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from notion_client import Client
 import time
 from pprint import pprint
 import requests
+from threading import Thread
 
 notion = Client(auth=os.environ["NOTION_TOKEN"])
 
@@ -14,7 +15,7 @@ CORS(app)
 #Axiom.ai
 axiom_api_key = os.environ["AXIOM_API_KEY"]
 axiom_name_to_clear_verification_sheet = "Clear Sheet - Kayak data from verification"
-axiom_name_to_scrap_kayak_results = "Scrap Kayak results"
+axiom_name_to_scrap_kayak_results = "Get Kayak promo details"
 
 #The Notion database "Vuelos" - https://www.notion.so/exploradordeviajes/76dea96154c14c2e9ee606de094f0c9c?v=3af9e9b1a8724910a86bda1ac8e90b36
 vuelos_database_id = "76dea961-54c1-4c2e-9ee6-06de094f0c9c"
@@ -22,6 +23,34 @@ vuelos_database_id = "76dea961-54c1-4c2e-9ee6-06de094f0c9c"
 #################################################
 ### 1. FETCHES THE TOP PROMOS BASED ON PROFIT ###
 #################################################
+def isPriceValidForRegion(region, price):
+  if region == "Europa" and price < 650:
+    return True
+  elif (region == "Sudamérica Norte" or region == "Estados Unidos Centro" or region == "México" or region == "Estados Unidos Este" or region == "Canadá") and price < 300:
+    return True
+  elif region == "Estados Unidos Oeste" and price < 600:
+    return True
+  elif region == "Sudamérica Sur" and price < 500:
+    return True
+  elif region == "África" and price < 800:
+    return True
+  elif (region == "Oceanía" or region == "Asia" or region == "Medio Oriente") and price < 1000:
+    return True
+  return False
+
+
+def validPromo(promo):
+  region = promo["properties"]["Región"]["rollup"]["array"][0]["select"]["name"]
+  price = promo["properties"]["price"]["number"]
+  return isPriceValidForRegion(region, price)
+
+
+def getPromosWithDesiredParameters(results):
+  verifiedResults = []
+  for result in results:
+    if validPromo(result):
+      verifiedResults.append(result)
+  return verifiedResults
 
 
 def fetchTopPromos(items_count):
@@ -52,7 +81,11 @@ def fetchTopPromos(items_count):
         "direction": "descending"
       }]
     })
-  return result["results"][:items_count]
+
+  all_results = result["results"]
+  validatedPromos = getPromosWithDesiredParameters(all_results)
+  print("From a total of", len(all_results), "results," , len(validatedPromos), "are valid and the maximum to return are", items_count)
+  return validatedPromos[:items_count]
 
 
 ###################################################
@@ -92,7 +125,8 @@ def clearVerificationSheet():
     "name": axiom_name_to_clear_verification_sheet,
     "data": [[""]]
   }
-  response = requestAxiomWithRetryAndDelay(request)
+  #Request Axiom. If axiom is busy, just ignore it
+  response = requests.post("https://lar.axiom.ai/api/v3/trigger", json=request)
   print("Response from Axiom cleaning sheet: ", response.text)
   
 
@@ -111,7 +145,7 @@ def verifyPromoInAxiom(verification_data):
   request = {
     "key": axiom_api_key,
     "name": axiom_name_to_scrap_kayak_results,
-    "data": [verification_data]
+    "data": verification_data
   }
 
   response = requestAxiomWithRetryAndDelay(request)
@@ -121,8 +155,9 @@ def verifyPromoInAxiom(verification_data):
 def verifyPromos(promos):
   clearVerificationSheet()
   promos_data_for_request = generateDataForRequest(promos)
-  for request_data in promos_data_for_request:
-    verifyPromoInAxiom(request_data)
+  print("promos_data_for_request")
+  print(promos_data_for_request)
+  verifyPromoInAxiom(promos_data_for_request)
 
 
 #######################################################################
@@ -132,6 +167,12 @@ def verifyPromos(promos):
 def callMigrator():
   requests.get("https://gs2notion-migrator.altancabal.repl.co/migrate")
 
+def startFetching():
+  #1. FETCHES THE TOP PROMOS BASED ON discountSpecialFunction
+  promos = fetchTopPromos(10)
+  #2. CALLS THE AXIOM THAT VERIFIES THE PROMOS
+  verifyPromos(promos)
+
 
 @app.route('/')
 def index():
@@ -140,17 +181,10 @@ def index():
 
 @app.route('/verify')
 def verify():
-  #1. FETCHES THE TOP PROMOS BASED ON PROFIT
-  promos = fetchTopPromos(10)
-  #2. CALLS THE AXIOM THAT VERIFIES THE PROMOS
-  verifyPromos(promos)
-
-  #3. CALLS THE REPLIT THAT MIGRATES FROM GOOGLE SHEETS TO NOTION
-  print("Waiting 5 minutes before migrating data from Google Sheets to Notion")
-  time.sleep(300)
-  callMigrator()
-  
-  return {"status": 200}
+  #Asyncronously start to fetch promos
+  Thread(target = startFetching).start()
+  #startFetching()
+  return {"message": "Started to run Axioms. This process will take long. Check the axiom extension to see more"}
 
 
-app.run(host='0.0.0.0', port=81)
+app.run(host='0.0.0.0', port=81, processes=1)
